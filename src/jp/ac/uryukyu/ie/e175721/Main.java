c5f813d4ad044e0f7cdada4a774960f86f3a2a55
Takuma Motohara <e175721@ie.u-ryukyu.ac.jp>
2017-11-18 13:25:36 +0900

    first commit

--- src/jp/ac/uryukyu/ie/e175721/Main.java
+++ src/jp/ac/uryukyu/ie/e175721/Main.java
@@ -2,7 +2,17 @@ package jp.ac.uryukyu.ie.e175721;
 
 public class Main {
     public static void main(String[] args) {
-        String str = null;
-        System.out.println(str.length());
+        try {
+            String str = null;
+            System.out.println(str.length());
+        }
+
+        catch (NullPointerException e) {
+            System.out.println("NullPointerExceptionが発生");
+            System.err.println(e.getMessage());
+
+        }
+
+
     }
 }
